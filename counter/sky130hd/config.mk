eb1ba21ddae45a42377946ffbfd6e2cd04c44e0d
Pawel Czarnecki <pczarnecki@antmicro.com>
2023-06-19 16:06:10 +0200

    [TEMP] tools: openroad: Add sample counter
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

---
+++ counter/sky130hd/config.mk
@@ -0,0 +1,12 @@
+export DESIGN_NICKNAME  = counter
+export DESIGN_NAME      = counter
+export PLATFORM         = sky130hd
+
+# Path relative to third_party/OpenROAD-flow-scripts/flow
+export VERILOG_FILES    = ../../../counter/counter.v
+export SDC_FILE         = ../../../counter/$(PLATFORM)/constraint.sdc
+
+export CORE_UTILIZATION = 40
+export PLACE_DENSITY    = 0.6
+export TNS_END_PERCENT  = 100
+
